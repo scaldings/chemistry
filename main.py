53e2f295041653561781b83b77a022f033ed13c1
Luke <lukas.zahoransky@zsbystricany.info>
2020-10-25 23:25:12 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import string
 
 
+def avogadrova_konstanta():
+    return 6.022 * pow(10, 23)
+
+
 def cisla():
     return '0123456789'
 
@@ -13,6 +17,13 @@ def velke_pismena():
     return string.ascii_uppercase
 
 
+def zaokruhelnie(cislo: float, miesta: int):
+    cislo_string = str(cislo)
+    desatinne_miesta = cislo_string.split('.')[1]
+    cislo_zaokruhlene = f'{cislo_string.split(".")[0]}.{desatinne_miesta[:miesta]}'
+    return cislo_zaokruhlene
+
+
 def atomova_hmotnost_prvku(prvok: str):
     file = open('prvky.txt', 'r')
     prvky, hmotnost = file.read().split('\n'), 0
@@ -23,42 +34,42 @@ def atomova_hmotnost_prvku(prvok: str):
 
 
 def vzorec_do_listu(vzorec: str):
-    temp_list, final_list, zvysok_zluceniny = list(vzorec), [], ''
+    temp_list, final_list, zvysok_molekuly = list(vzorec), [], ''
     if temp_list[0] in cisla():
         final_list.append(temp_list[0])
-        zvysok_zluceniny = vzorec[1:]
+        zvysok_molekuly = vzorec[1:]
     else:
         final_list.append('1')
-        zvysok_zluceniny = vzorec
+        zvysok_molekuly = vzorec
 
-    for x in range(0, len(zvysok_zluceniny) - 1):
-        zvysok_zluceniny_list = list(zvysok_zluceniny)
-        if zvysok_zluceniny_list[x] in velke_pismena():
-            if zvysok_zluceniny_list[x + 1] in male_pismena():
-                if zvysok_zluceniny_list[x + 2] in cisla():
-                    to_append = zvysok_zluceniny_list[x] + zvysok_zluceniny_list[x + 1] + zvysok_zluceniny_list[x + 2]
+    for x in range(0, len(zvysok_molekuly) - 1):
+        zvysok_molekuly_list = list(zvysok_molekuly)
+        if zvysok_molekuly_list[x] in velke_pismena():
+            if zvysok_molekuly_list[x + 1] in male_pismena():
+                if zvysok_molekuly_list[x + 2] in cisla():
+                    to_append = zvysok_molekuly_list[x] + zvysok_molekuly_list[x + 1] + zvysok_molekuly_list[x + 2]
                     final_list.append(to_append)
-                    zvysok_zluceniny.replace(to_append, '')
+                    zvysok_molekuly.replace(to_append, '')
                 else:
-                    to_append = zvysok_zluceniny_list[x] + zvysok_zluceniny_list[x + 1]
+                    to_append = zvysok_molekuly_list[x] + zvysok_molekuly_list[x + 1]
                     final_list.append(to_append)
-                    zvysok_zluceniny.replace(to_append, '')
+                    zvysok_molekuly.replace(to_append, '')
             else:
-                if zvysok_zluceniny_list[x + 1] in cisla():
-                    to_append = zvysok_zluceniny_list[x] + zvysok_zluceniny_list[x + 1]
+                if zvysok_molekuly_list[x + 1] in cisla():
+                    to_append = zvysok_molekuly_list[x] + zvysok_molekuly_list[x + 1]
                     final_list.append(to_append)
-                    zvysok_zluceniny.replace(to_append, '')
+                    zvysok_molekuly.replace(to_append, '')
                 else:
-                    to_append = zvysok_zluceniny_list[x]
+                    to_append = zvysok_molekuly_list[x]
                     final_list.append(to_append)
-                    zvysok_zluceniny.replace(to_append, '')
+                    zvysok_molekuly.replace(to_append, '')
     return final_list
 
 
-def hmotnost_zluceniny(zlucenina: list):
-    pocet_zlucenin = zlucenina[0]
+def hmotnost_molekuly(molekula: list):
+    pocet_molekul = molekula[0]
     hmotnost, i = 0, 0
-    for x in zlucenina:
+    for x in molekula:
         if i != 0:
             if x in male_pismena():
                 if x in cisla():
@@ -73,19 +84,24 @@ def hmotnost_zluceniny(zlucenina: list):
                     pocet_atomov = 1
                 hmotnost += float(pocet_atomov) * atomova_hmotnost_prvku(x[:1])
         i += 1
-    return hmotnost * float(pocet_zlucenin)
+    return hmotnost * float(pocet_molekul)
 
 
-def zaokruhelnie(cislo: float, miesta: int):
-    cislo_string = str(cislo)
-    desatinne_miesta = cislo_string.split('.')[1]
-    cislo_zaokruhlene = f'{cislo_string.split(".")[0]}.{desatinne_miesta[:miesta]}'
-    return cislo_zaokruhlene
+def latkove_mnozstvo(hmotnost_vzorky: float, molarna_hmotnost: float):
+    return hmotnost_vzorky / molarna_hmotnost
+
+
+def latkove_mnozstvo2(celkovy_pocet_castic: float):
+    return celkovy_pocet_castic / avogadrova_konstanta()
+
+
+def latkova_koncentracia(latkove_mnoz: float, objem: float):
+    return latkove_mnoz / objem
 
 
 def main():
     vzorec = str(input('Zadaj vzorec zlúčeniny: '))
-    print(zaokruhelnie(hmotnost_zluceniny(vzorec_do_listu(vzorec)), 2))
+    print(zaokruhelnie(hmotnost_molekuly(vzorec_do_listu(vzorec)), 2))
     main()
 
 
